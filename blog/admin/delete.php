a2a282e05fd74b8bdd422f4fd962386d61d0b9ee
Wunna <wana192770@gmail.com>
2021-01-13 00:16:38 +0630

    delete blog folder

--- blog/admin/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require '../config/config.php';
-
-    $stmt = $pdo->prepare("DELETE FROM posts WHERE id=".$_GET['id']);
-    $stmt->execute();
-
-    header('location: index.php');
-?>
--- blog/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $options = array(
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    );
-
-    $pdo = new PDO("mysql:dbname=blogapp;host=localhost",'root','');
-?>
